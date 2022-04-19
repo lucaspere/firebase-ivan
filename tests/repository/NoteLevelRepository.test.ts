15799b60077f0ed37787ac58a3c054c38ac80407
Lucas Pereira <lucasrupestre@hotmail.com>
2022-04-18 21:33:28 -0300

    test: add leveldb clear method test

--- tests/repository/NoteLevelRepository.test.ts
+++ tests/repository/NoteLevelRepository.test.ts
@@ -18,6 +18,28 @@ suite('Testing levelDB Repository', function () {
         await levelDB.clear();
     });
 
+    test('Should clear database', async () => {
+        const note1 = new Note('Note Test', 'Testing Level DB Repository');
+        const note2 = new Note('Note Test 2', 'Testing Level DB Repository 2');
+        await levelDB.batch([
+            {
+                type: 'put',
+                key: note1.id,
+                value: note1,
+            },
+            {
+                type: 'put',
+                key: note2.id,
+                value: note2,
+            },
+        ]);
+        await noteRepository.clear();
+
+        const notes = await levelDB.values().all();
+
+        assert.isFalse(!!notes.length);
+    });
+
     test('Should save a note', async () => {
         const newNote = new Note('Note Test', 'Testing Level DB Repository');
         const note = await noteRepository.create(newNote);

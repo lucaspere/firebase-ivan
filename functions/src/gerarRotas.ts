4b1c2f6fdbfc2840f5aea1199102baf66aaab0e9
lucaspere <lucasrupestre@hotmail.com>
2022-06-11 18:30:48 -0300

    refac: create gerarRotas file

--- functions/src/gerarRotas.ts
+++ functions/src/gerarRotas.ts
@@ -4,11 +4,22 @@ import {
   UnitSystem,
 } from "@googlemaps/google-maps-services-js";
 import * as functions from "firebase-functions";
+import { push, ref } from "firebase/database";
 import { inspect } from "util";
 import { client } from ".";
+import { realTimeDB as db } from "./database";
 
-interface GerarRotaResponse extends Payload {
-  id: string;
+interface X {
+  endereco: string;
+  placeId: string;
+  latLng: LatLng;
+}
+interface GerarRotaResponse {
+  nome: string;
+  origem: X;
+  destino: X;
+  distancia: string;
+  duracao: string;
 }
 interface Payload {
   nome: string;
@@ -16,37 +27,44 @@ interface Payload {
   destino: LatLng;
 }
 
-export default async (req: Request, res: functions.Response<Payload>) => {
-  functions.logger.info(`Recebendo o seguinte payload ${inspect(req.body)}`, {
-    structuredData: true,
-  });
+export default async (
+  req: functions.Request,
+  res: functions.Response<Payload>
+): Promise<void> => {
+  if (req.method === "POST") {
+    functions.logger.info(`Recebendo o seguinte payload ${inspect(req.body)}`, {
+      structuredData: true,
+    });
 
-  const { origem, destino, nome } = req.body as unknown as Payload;
-  const { data } = await client.directions({
-    params: {
-      origin: origem,
-      destination: destino,
-      key: process.env.GOOGLE_MAPS_KEY!,
-      mode: TravelMode.driving,
-      units: UnitSystem.metric,
-    },
-  });
-  const routeData = data.routes[0].legs[0];
-  const route = {
-    nome,
-    origem: {
-      endereco: routeData.start_address,
-      placeId: data.geocoded_waypoints[0].place_id,
-      latLng: [routeData.start_location.lat, routeData.start_location.lng],
-    },
-    destino: {
-      endereco: routeData.end_address,
-      placeId: data.geocoded_waypoints[1].place_id,
-      latLng: [routeData.end_location.lat, routeData.end_location.lng],
-    },
-    distancia: routeData.distance,
-    duracao: routeData.duration,
-  };
+    const { origem, destino, nome } = req.body as unknown as Payload;
+    const { data } = await client.directions({
+      params: {
+        origin: origem,
+        destination: destino,
+        key: process.env.GOOGLE_MAPS_KEY!,
+        mode: TravelMode.driving,
+        units: UnitSystem.metric,
+      },
+    });
+    const routeData = data.routes[0].legs[0];
+    const route: GerarRotaResponse = {
+      nome,
+      origem: {
+        endereco: routeData.start_address,
+        placeId: data.geocoded_waypoints[0].place_id,
+        latLng: [routeData.start_location.lat, routeData.start_location.lng],
+      },
+      destino: {
+        endereco: routeData.end_address,
+        placeId: data.geocoded_waypoints[1].place_id,
+        latLng: [routeData.end_location.lat, routeData.end_location.lng],
+      },
+      distancia: routeData.distance.text,
+      duracao: routeData.duration.text,
+    };
 
-  res.status(201).send();
+    await push(ref(db, `/rotas/${nome}`), route);
+
+    res.status(201).send();
+  } else res.status(405).send();
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,43 +1,10 @@
 import * as functions from "firebase-functions";
-import { inspect } from "util";
-
-import {
-  Client,
-  LatLng,
-  TravelMode,
-  UnitSystem,
-} from "@googlemaps/google-maps-services-js";
+import { Client } from "@googlemaps/google-maps-services-js";
 import { realTimeDB as db } from "./database";
-import { connectDatabaseEmulator, push, ref } from "firebase/database";
+import { connectDatabaseEmulator } from "firebase/database";
+import gerarRotas from "./gerarRotas";
 
-const client = new Client({});
+export const client = new Client({});
 connectDatabaseEmulator(db, "127.0.0.1", 9000);
 
-interface Payload {
-  nomeRota: string;
-  origem: LatLng;
-  destino: LatLng;
-}
-
-export const gereRota = functions.https.onRequest(async (req, res) => {
-  if (req.method === "POST") {
-    functions.logger.info(`Recebendo o seguinte payload ${inspect(req.body)}`, {
-      structuredData: true,
-    });
-
-    const { nomeRota, origem, destino } = req.body as Payload;
-    const { data } = await client.directions({
-      params: {
-        origin: origem,
-
-        destination: destino,
-        key: process.env.GOOGLE_MAPS_KEY!,
-        mode: TravelMode.driving,
-        units: UnitSystem.metric,
-      },
-    });
-
-    await push(ref(db, "/rotas/" + nomeRota), data);
-    res.send(data);
-  } else res.status(405).send();
-});
+export const gerarRota = functions.https.onRequest(gerarRotas);
